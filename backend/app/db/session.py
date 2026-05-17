ad823ea5d62d2c25dbc45c21651d4ab6a2f4dc8d
critical_error <nikula2108@gmail.com>
2026-05-17 18:42:59 +0300

    feat(backend): implement auto initialization of the table with roles in the absence of records.

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -1,3 +1,4 @@
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 
 from app.core.config import settings
@@ -12,6 +13,20 @@ AsyncSessionLocal = async_sessionmaker(
 async def init_db_and_tables():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
+
+    from app.models.role import Role
+
+    async with AsyncSessionLocal() as session:
+        result = await session.execute(select(Role))
+        if result.scalars().first() is None:
+            session.add_all(
+                [
+                    Role(name="Reader"),
+                    Role(name="Librarian"),
+                    Role(name="Admin"),
+                ]
+            )
+            await session.commit()
 
 
 async def get_session():
